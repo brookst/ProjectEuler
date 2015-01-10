21fba60bd9fe65c452dbc35b5faa87de1ae81d7e
Tim Brooks <brooks@cern.ch>
2015-01-10 21:43:04 +0000

    Revise problem1 a little

--- src/problem1.rs
+++ src/problem1.rs
@@ -3,11 +3,9 @@ extern crate test;
 
 fn problem1() -> isize {
     let mut x: isize = 0;
-    let mut a: usize = 0;
-    let v: Vec<isize> = (0is..1001).collect();
-    for _i in (0is..v.len() as isize) {
-        if v[a] % 5 == 0 || v[a] % 3 == 0 { x += v[a]; }
-        a += 1;
+    let v: Vec<isize> = (0is..1000).collect();
+    for i in (0..v.len()) {
+        if v[i] % 5 == 0 || v[i] % 3 == 0 { x += v[i]; }
     }
     x
 }
