97f8bfa370bf6505c2b711bbf290bef3af645b5e
Tim Brooks <brooks@cern.ch>
2015-01-10 21:12:43 +0000

    Update syntax for rust 1.0.0-alpha

--- Problem1.rs
+++ Problem1.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let mut x: int = 0;
+    let mut x: isize = 0;
     let mut a: usize = 0;
-    let mut v: Vec<int> = (range(0i, 1001i).collect());
-    for _i in range(0i, v.len() as int) {
+    let v: Vec<isize> = (0is..1001).collect();
+    for _i in (0is..v.len() as isize) {
         if v[a] % 5 == 0 || v[a] % 3 == 0 { x += v[a]; }
         a += 1;
-    } 
+    }
     println!("{}, {:?}", x, v);
 }
