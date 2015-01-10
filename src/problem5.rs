850841dc0b004f9bcdde40a8ae7f40502cd90f7b
Tim Brooks <brooks@cern.ch>
2015-01-10 23:34:25 +0000

    Clean up problem5

--- src/problem5.rs
+++ src/problem5.rs
@@ -2,16 +2,13 @@
 extern crate test;
 
 fn problem5() -> isize {
-    let mut ph = 1is;
     let mut i = 20is;
-    while ph==1 {
+    loop {
         if i % 11 == 0 && i % 12 == 0 && i % 13 == 0 && i % 14 == 0 && i % 15 == 0 && i % 16 == 0 && i % 17 == 0 && i % 18 == 0 && i % 19 == 0 {
             return i;
-            ph = 2;
         }
         i += 20;
     }
-    i
 }
 
 pub fn main() {
