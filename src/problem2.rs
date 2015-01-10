a330c3bcfc7e2a4ecfcc1ef809d456c48fcf49a8
Tim Brooks <brooks@cern.ch>
2015-01-10 22:12:13 +0000

    Upgrade problem2 to rust 1.0.0-alpha syntax

--- src/problem2.rs
+++ src/problem2.rs
@@ -2,14 +2,14 @@
 extern crate test;
 
 fn problem2() -> isize {
-    let mut x: int = 0;
+    let mut x: isize = 0;
     let mut a: usize = 0;
-    let mut v = vec!(1i , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55);
-    for _i in range(0i, 23i) {
+    let mut v = vec!(1is , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55);
+    for _i in (0is..23) {
         let x = (v[v.len() - 2] + v[v.len() - 1]) as isize;
         v.push(x);
     }
-    for _i in range(0i, v.len() as int) {
+    for _i in (0is..v.len() as isize) {
         if v[a] % 2 == 0 { x += v[a]; }
         a += 1;
     }
