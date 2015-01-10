ae7585b3cb4cb8f653b4eccae2be5ac7f900a731
Tim Brooks <brooks@cern.ch>
2015-01-10 21:59:30 +0000

    Add test and benchmark to problem2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 pub mod problem1;
+pub mod problem2;
 
 fn main() {
     problem1::main();
+    problem2::main();
 }
--- src/problem2.rs
+++ src/problem2.rs
@@ -1,4 +1,7 @@
-fn main() {
+#[cfg(test)]
+extern crate test;
+
+fn problem2() -> isize {
     let mut x: int = 0;
     let mut a: usize = 0;
     let mut v = vec!(1i , 2 , 3 , 5 , 8 , 13 , 21 , 34 , 55);
@@ -9,6 +12,21 @@ fn main() {
     for _i in range(0i, v.len() as int) {
         if v[a] % 2 == 0 { x += v[a]; }
         a += 1;
-    } 
-    println!("{}, {:?}", x, v);
+    }
+    x
+}
+
+pub fn main() {
+    println!("Problem2: {}", problem2());
+}
+
+#[test]
+fn test2() {
+    main();
+    assert!(problem2() == 4613732is);
+}
+
+#[bench]
+fn bench2(b: &mut test::Bencher) {
+    b.iter(|| problem2());
 }
