0bd405eb6a8df8bea81ef6caca4ccb8d8fe31a22
Tim Brooks <brooks@cern.ch>
2015-01-10 23:08:15 +0000

    Add test and bench to problem5

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod problem1;
 pub mod problem2;
 pub mod problem3;
+pub mod problem5;
 
 fn main() {
     problem1::main();
     problem2::main();
     problem3::main();
+    problem5::main();
 }
--- src/problem5.rs
+++ src/problem5.rs
@@ -1,11 +1,30 @@
-fn main(){
+#[cfg(test)]
+extern crate test;
+
+fn problem5() -> isize {
     let mut ph = 1is;
     let mut i = 1is;
     while ph==1 {
         if i % 2 == 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0 && i % 6 == 0 && i % 7 == 0 && i % 8 == 0 && i % 9 == 0 && i % 10 == 0 && i % 11 == 0 && i % 12 == 0 && i % 13 == 0 && i % 14 == 0 && i % 15 == 0 && i % 16 == 0 && i % 17 == 0 && i % 18 == 0 && i % 19 == 0 && i % 20 == 0 {
-            println!("{} is your answer", i);
+            return i;
             ph = 2;
-            }
-    i += 1;
+        }
+        i += 1;
+    }
+    i
 }
+
+pub fn main(){
+    println!("Problem5: {}", problem5());
+}
+
+#[test]
+fn test5() {
+    main();
+    assert!(problem5() == 232792560is);
+}
+
+#[bench]
+fn bench5(b: &mut test::Bencher) {
+    b.iter(|| problem5());
 }
