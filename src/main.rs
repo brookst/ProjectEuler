a4a258bfae6f7179c62ca9a58407ce10cd1a59b6
Tim Brooks <brooks@cern.ch>
2015-01-11 00:49:50 +0000

    Solve problem4

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 pub mod problem1;
 pub mod problem2;
 pub mod problem3;
+pub mod problem4;
 pub mod problem5;
 
 fn main() {
     problem1::main();
     problem2::main();
     problem3::main();
+    problem4::main();
     problem5::main();
 }
---
+++ src/problem4.rs
@@ -0,0 +1,43 @@
+#[cfg(test)]
+extern crate test;
+
+fn is_palindrome(n: isize) -> bool {
+    let chars: Vec<char> = format!("{}", n).chars().collect();
+    let max = chars.len() - 1;
+    for i in (0..chars.len()) {
+        if chars[i] != chars[max - i] {
+            return false;
+        }
+    }
+    true
+}
+
+fn problem4() -> isize {
+    let mut largest = 0is;
+    for i in (100..999) {
+        for j in (100..999) {
+            let candidate = i * j;
+            if is_palindrome(candidate) && candidate > largest {
+                largest = candidate;
+            }
+        }
+    }
+    largest
+}
+
+pub fn main() {
+    println!("Problem4: {}", problem4());
+}
+
+#[test]
+fn test4() {
+    main();
+    assert!(is_palindrome(9009) == true);
+    assert!(is_palindrome(9010) == false);
+    assert!(problem4() == 906609is);
+}
+
+#[bench]
+fn bench4(b: &mut test::Bencher) {
+    b.iter(|| problem4());
+}
